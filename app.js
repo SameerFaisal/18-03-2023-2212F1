5fc034ad4751bf3e03f0bdbfc52511888b49644e
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-03-18 19:00:51 +0500

    18-03-2023

---
+++ app.js
@@ -0,0 +1,62 @@
+// $(document).ready(function(){
+//     $(".hide").click(function(){
+//        $("#p1").hide(5000)
+//     })
+// })
+
+// $(document).ready(function(){
+//     $(".show").click(function(){
+//        $("#p1").show(7000)
+//     })
+// })
+
+// $(document).ready(function(){
+//     $(".hide").click(function(){
+//          $("#p1").fadeOut(5000)
+//     })
+// })
+
+// $(document).ready(function(){
+//     $(".show").click(function(){
+//          $("#p1").fadeIn(5000)
+//     })
+// })
+
+// $(document).ready(function(){
+//     $(".tg").click(function(){
+//        $("#p1").toggle(5000)
+//     })
+// })
+
+// $(document).ready(function(){
+//     $(".tg").click(function(){
+//        $("#p1").fadeToggle(5000)
+//     })
+// })
+
+// $(document).ready(function(){
+//     $(".hide").click(function(){
+//        $("img").fadeOut()
+//     })
+// })
+
+// $(document).ready(function(){
+//     $(".show").click(function(){
+//        $("img").fadeIn()
+//     })
+// })
+
+$(document).ready(function(){
+    $(".hide").click(function(){
+       $("div").fadeOut()
+    })
+})
+
+$(document).ready(function(){
+    $(".show").click(function(){
+       $("div").fadeIn()
+    })
+})
+
+
+
